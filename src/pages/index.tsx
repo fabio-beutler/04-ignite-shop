836d8c3f7e0aeb0ecfb6b96dfcbeb8296cd755dc
Fabio Beutler <dev.fabiobeutler@gmail.com>
2023-05-01 01:43:06 -0400

    Estrutura visual - Estilos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,13 @@
 import { styled } from '@/styles';
 import Head from 'next/head';
 
-const Button = styled('button', {
-  backgroundColor: '$rocketseat',
-});
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Ignite Shop</title>
       </Head>
-      <Button>Enviar</Button>
+      <h1>home</h1>
     </>
   );
 }
