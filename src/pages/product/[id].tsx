419439aa4c3481c5787e5e20036b784350e744ad
Fabio Beutler <dev.fabiobeutler@gmail.com>
2023-05-01 17:28:29 -0400

    Produto & Checkout - Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,3 +1,29 @@
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+
+import { ImageContainer, ProductContainer, ProductDetails } from '@/styles/pages/product';
+
 export default function Product() {
-  return <h1>Product</h1>;
+  const {
+    query: { id },
+  } = useRouter();
+
+  return (
+    <ProductContainer>
+      <ImageContainer>{/* <Image  /> */}</ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident reiciendis
+          porro ipsum quas recusandae! Molestiae, distinctio. Nobis doloribus, sequi, ea
+          eveniet mollitia veniam velit animi tempore facilis tempora explicabo totam?
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
